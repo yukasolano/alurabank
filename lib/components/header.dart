23f48ce594e32bfc2faf95acca6f4b1ddff92fba
Yuka Kyushima <yuka.kyushima@nubank.com.br>
2023-02-21 14:37:00 -0300

    Create header

---
+++ lib/components/header.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(103, 99, 234, 1),
+              Color.fromRGBO(155, 105, 254, 1),
+              Color.fromRGBO(195, 107, 255, 1),
+            ],
+          ),
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(10))),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 80, 20, 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                  text: const TextSpan(
+                    text: '\$ ',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: '1000.00',
+                        style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                const Text(
+                  "Available balance",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                )
+              ],
+            ),
+            const Icon(
+              Icons.account_circle,
+              size: 35,
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../components/header.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [Header()],),
+    );
+  }
+}
