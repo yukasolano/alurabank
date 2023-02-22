da7d5a02b0353fc9485991dc7029bc6df35f525f
Yuka Kyushima <yuka.kyushima@nubank.com.br>
2023-02-22 15:22:25 -0300

    Add theme

--- lib/themes/my_theme.dart
+++ lib/themes/my_theme.dart
@@ -5,6 +5,7 @@ ThemeData myTheme = ThemeData(
   primaryColor: ThemeColors.primaryColor,
   primarySwatch: ThemeColors.primaryColor,
   brightness: Brightness.dark,
+  fontFamily: "Raleway",
   textTheme: const TextTheme(
       bodyMedium: TextStyle(fontSize: 16),
       bodyLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
